d90bcffab8650efb29edb8dcdaaf9c2e7347e8c1
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-09-24 08:48:49 +0530

    first commit

---
+++ src/main/java/com/example/BookMyShowCaseStudy/BookMyShowCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShowCaseStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowCaseStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookMyShowCaseStudy/BookMyShowCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShowCaseStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
